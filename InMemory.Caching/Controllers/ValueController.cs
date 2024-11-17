5a2027ddd41120f900edb77984bd0d8327ce938c
hsnovfrmayil <fermayilhesenov@Fermayils-MacBook-Air.local>
2024-11-17 20:36:42 +0400

    redis in memory cache and distributed cache

---
+++ InMemory.Caching/Controllers/ValueController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemory.Caching.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+public class ValueController :ControllerBase
+{
+    readonly IMemoryCache _memoryCache;
+
+    public ValueController(IMemoryCache memoryCache)
+    {
+        _memoryCache = memoryCache;
+    }
+
+
+    [HttpGet("set/{name}")]
+    public void Set(string name)
+    {
+        _memoryCache.Set("name", name);
+    }
+
+
+    [HttpGet("[action]")] 
+    public string Get()
+    {
+        if(_memoryCache.TryGetValue<string>("name",out string name))
+        {
+            return name.Substring(3);
+        }
+        return "";
+    }
+}
+
